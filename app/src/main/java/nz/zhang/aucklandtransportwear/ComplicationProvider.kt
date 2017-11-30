a98b27762420a8a7dd7d5e3ca26bf1208d221a43
encryptededdy <pcman2000@gmail.com>
2017-12-01 00:09:36 +1300

    Complication selection complete

--- app/src/main/java/nz/zhang/aucklandtransportwear/ComplicationProvider.kt
+++ app/src/main/java/nz/zhang/aucklandtransportwear/ComplicationProvider.kt
@@ -7,17 +7,33 @@ import android.support.wearable.complications.ComplicationData
 import android.support.wearable.complications.ComplicationManager
 import android.support.wearable.complications.ComplicationProviderService
 import android.support.wearable.complications.ComplicationText
+import nz.zhang.aucklandtransportwear.wakaapi.StopType
 
 class ComplicationProvider : ComplicationProviderService() {
     override fun onComplicationUpdate(complicationID: Int, dataType: Int, complicationManager: ComplicationManager?) {
+        // Get the stop
+        val stop = DataStore.complicationStop
+        if (stop == null) { // do nothing if no stop is selected
+            complicationManager?.noUpdateRequired(complicationID)
+            return
+        }
         // Build the intent we will use to open the stop
         val stopIntent = Intent(applicationContext, StopActivity::class.java)
-        stopIntent.putExtra("stop", DataStore.savedStops[0])
+        stopIntent.putExtra("stop", stop)
         val pendingIntent = PendingIntent.getActivity(this, 0, stopIntent, 0)
+
+        val icon: Icon
+        // Get the icon we need
+        icon = when (stop.stopType()) {
+            StopType.TRAIN -> Icon.createWithResource(applicationContext, R.drawable.ic_train_white_24dp)
+            StopType.BUS -> Icon.createWithResource(applicationContext, R.drawable.ic_directions_bus_white_24dp)
+            else -> Icon.createWithResource(applicationContext, R.drawable.ic_directions_boat_white_24dp)
+        }
+
         // We don't actually need to send live updates, we just need to set this up
         val complicationData = ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
-                .setShortText(ComplicationText.plainText("133"))
-                .setIcon(Icon.createWithResource(applicationContext, R.drawable.ic_train_white_24dp))
+                .setShortText(ComplicationText.plainText(stop.stop_id))
+                .setIcon(icon)
                 .setTapAction(pendingIntent)
                 .build()
 
