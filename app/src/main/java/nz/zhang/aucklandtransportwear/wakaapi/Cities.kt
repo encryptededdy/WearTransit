715aace7f97551e7c1763975db82555550738e55
Edward Zhang <edward@zhang.nz>
2021-09-28 00:38:25 +1300

    Added Christchurch support, updated gitignore

--- app/src/main/java/nz/zhang/aucklandtransportwear/wakaapi/Cities.kt
+++ app/src/main/java/nz/zhang/aucklandtransportwear/wakaapi/Cities.kt
@@ -2,10 +2,10 @@ package nz.zhang.aucklandtransportwear.wakaapi
 
 enum class Cities (val cityName: String, val shortCode: String, val defaultLat: Double, val defaultLong: Double){
     AUCKLAND("Auckland", "nz-akl", -36.844, 174.766),
-    WELLINGTON("Wellington", "nz-wlg", -41.279, 174.779);
+    WELLINGTON("Wellington", "nz-wlg", -41.279, 174.779),
+    CHRISTCHURCH("Christchurch", "nz-chc", -43.532, 172.634);
 
     override fun toString(): String {
         return cityName
     }
-
 }
